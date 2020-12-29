103346d5f34f5b87f1193bf930e765665624dc7e
Carolanne <carolanne.fuchs@gmail.com>
2020-12-29 10:20:10 +0000

    Route handler

--- handler.py
+++ handler.py
@@ -1,19 +1,32 @@
 import json
+import os
 
 
 def hello(event, context):
-    # if event.get("resource") == "/":
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        # "input": event.queryStringParameters,
-        "event": event
-        # "context": context
-    }
+            "message": "Go Serverless v1.0! Your function executed successfully!",
+            # "input": event.queryStringParameters,
+            "event": event
+            # "context": context
+        }
+    # print('Body default, not if statement')
+    print(os.environ['IS_OFFLINE'])
+    # print(event.get("resource"))
+    # print(event["resource"])
+    
+    if os.environ['IS_OFFLINE']:
+        if event['resource'] == "/dev/users/create":
+            body = {
+                "message": "Hello World"
+            }
+            print('Body default, not if statement, IS_OFFLINE = true')
 
-    # elif event.get("resource") == "/users/create":
-    #     body = {
-    #         "message": "Hello World"
-    #     }
+    else:
+        if event["resource"] == "/users/create":
+            body = {
+                "message": "Hello World"
+            }
+            print('Body default, not if statement, remote')
 
     response = {
         "statusCode": 200,
