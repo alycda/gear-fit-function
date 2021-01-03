c60cf72c7c97382445af2b4a69290ce21290dc72
Carolanne <carolanne.fuchs@gmail.com>
2021-01-03 06:46:26 +0000

    added user api endpoints

--- handler.py
+++ handler.py
@@ -31,11 +31,11 @@ def hello(event, context):
         )
         print("MySQL connection open")
         cursor = conn.cursor() 
+        nt_cursor = conn.cursor(named_Tuple = True)
 
         qSP = event['queryStringParameters']
 
-        if event['resource'] == root + "users/create":
-            #if query isn't null, write it to fc_users in the db
+        if event['resource'] == root + "users/create": #if query isn't null, write new record to fc_users in the db
             if qSP:
                 #body = the_govnuh("My bust size is " + qSP["bust"],{"input": qSP}) #original test message, replaced with db message below
                 user_bust = qSP["bust"]
@@ -44,16 +44,78 @@ def hello(event, context):
                 cursor.execute(sql)
                 conn.commit()
                 body = the_govnuh("My bust is " + user_bust + " inches, and my waist is " + user_waist + " inches. Writing to users.")
-                #need to add fit report values to fit_reports table.
-                #after all user-supplied values are added, close db connection. Will reopen it to return results but this will get it to update autoincremented id's.
-                #cursor.close()
-                #conn.close()
-
-                #need to add a function to return best fit results here
-
-
+                #Call fr/create here if qSP has fr_info                
             else: 
                 body = the_govnuh("Missing qSP")
+
+        if event['resource'] == root + "users/get-many": #returns all data for user id's in a given range
+            if qSP:
+                id_mn = qSP["min"]
+                id_max = qSP["max"]
+                sql = "SELECT * FROM fc_users WHERE user_id BETWEEN %s AND %s;"%(id_min,id_max)
+                nt_cursor.execute(sql)
+                res = nt_cursor.fetchmany() #res should be a list of named tuples, with each column name as the index
+                for row in res:
+                    user_id = row.user_id
+                    user_bust = row.user_bust
+                    user_waist = row.user_waist
+                body = the_govnuh("The user id is " + user_id + ", the user bust is " + user_bust + ", and the user waist is " + user_waist + "./n")
+            else:
+                body = the_govnuh("Missing qSP")
+
+        if event['resource'] == root + "users/get-one": #returns all data for a given user id
+            if qSP:
+                user = qSP["id"]
+                sql = "SELECT * FROM fc_users WHERE user_id = %s;"%(user)
+                nt_cursor.execute(sql)
+                res = nt_cursor.fetchone() #res should be a list of named tuples, with each column name as the index
+                for row in res:
+                    user_id = row.user_id
+                    user_bust = row.user_bust
+                    user_waist = row.user_waist
+                body = the_govnuh("The user id is " + user_id + ", the user bust is " + user_bust + ", and the user waist is " + user_waist + "./n")
+            else:
+                body = the_govnuh("Missing qSP")
+
+        if event['resource'] == root + "users/update": #updates any given values for a (mandatory) given user id
+            if qSP:
+                user_id = qSP["id"]
+                if qSP["bust"] and qSP["waist"]:
+                    user_bust = qSP["bust"]
+                    user_waist = qSP["waist"]
+                    sql = "UPDATE fc_users SET user_bust = %s AND user_waist = %s WHERE user_id = %s;"%(user_bust,user_waist,user_id) 
+                elif qSP["bust"]:
+                    user_bust = qSP["bust"]
+                    sql = "UPDATE fc_users SET user_bust = %s WHERE user_id = %s;"%(user_bust,user_id) 
+                elif qSP["waist"]:
+                    user_waist = qSP["waist"]
+                    sql = "UPDATE fc_users SET user_waist = %s WHERE user_id = %s;"%(user_waist,user_id) 
+                cursor.execute(sql)
+                conn.commit()
+                body = the_govnuh("The user id is " + user_id + ", the new user bust is " + user_bust + ", and the new user waist is " + user_waist + "./n")
+
+                #would be fun to set up with **kwargs, but the AND in sql queries gets messier than just handling 3 if statements
+                # kwargs = {"user_bust":qSP["bust"],"user_waist":qSP["waist"]}
+                # def user_updater(user_id, **kwargs):
+                #     sql = "UPDATE users SET "
+                #     for kw, arg in kwargs:
+                #         sql += ("%s = %s "%(kw,arg))
+                #     sql.append( "WHERE user_id = %s"%user_id)
+                #     return sql
+                # query_builder(user_id, kwargs)
+            else:
+                body = the_govnuh("Missing qSP")
+
+        if event['resource'] == root + "users/delete": #deletes a single user identified by a given user id
+            if qSP:
+                user_id = qSP["id"]
+                sql = "DELETE FROM fc_users WHERE user_id = %s LIMIT ONE;"%(user) #LIMIT ONE just to limit the damage if SQL accepts a wildcard for user id
+                cursor.execute(sql)
+                conn.commit()
+                body = the_govnuh("User " + user_id + " has been deleted from the database./n")
+            else:
+                body = the_govnuh("Missing qSP")
+
 
         response = {
             "statusCode": 200,
